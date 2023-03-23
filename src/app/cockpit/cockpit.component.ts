9cf3722378a758a8a0aa8560f7b02d1cca18918a
mojzi95 <toth.tamas19950628@gmail.com>
2023-03-23 08:38:18 +0100

    assigning an alias to custom property

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent {
+
+
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+
+  }
+
+  onAddBlueprint() {
+
+  }
+}
